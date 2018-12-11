80190b378bb115350fda50ea4120cfb1e418fd3a
Gloria <wanhuiqing18@mails.ucas.ac.cn>
2018-12-11 15:20:46 +0000

    meiyou

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "index#index"
   get '/signup', to: 'index#signup'
-<<<<<<< HEAD
-=======
 
->>>>>>> f4219585ae167ffab65ca77a4b57850f217a2740
+
   post '/upload', to: 'drivers#upload'
   get '/show', to: 'drivers#show'
   get '/edit', to: 'drivers#edit'
@@ -18,13 +16,10 @@ Rails.application.routes.draw do
   get '/taken_order', to: 'drivers#taken_order'
   get '/finished_order', to: 'drivers#finished_order'
   
-<<<<<<< HEAD
+  post '/upload', to: "driver#upload"
   
   post '/upload', to: 'driver#upload'
 
-=======
-  post '/upload', to: "driver#upload"
->>>>>>> f4219585ae167ffab65ca77a4b57850f217a2740
   get '/login', to: "sessions#new"
   post '/login', to: 'sessions#create'
   #delete  '/logout', to: 'sessions#destroy'

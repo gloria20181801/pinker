a06bb3d0b952caa76c56fc2fb9775d349f41915a
zwk2018 <zhangwenkai18@mails.ucas.ac.cn>
2018-12-12 15:55:34 +0000

    heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
-require 'nlpir'
-include Nlpir
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   @@driver_id_card_default = 'drivers/id_card/driver1@example.com.png'
@@ -7,6 +6,7 @@ class ApplicationController < ActionController::Base
   @@driver_head_default = 'drivers/head/driver1@example.com.png'
   @@student_id_card_default = 'students/id_card/s1@example.com.png'
   @@student_head_default = 'students/head/s1@example.com.png'
+  
   include SessionsHelper
   def search_params
     params.require(:search).permit(
